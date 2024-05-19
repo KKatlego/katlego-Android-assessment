5e1b6b12c93ab20b7465f4d1e4f5eab8fde3e21d
Katlego <katlego.code@gmail.com>
2024-05-20 00:34:37 +0200

    Developed a simple card view for engineer profile

--- app/src/main/java/com/glucode/about_you/about/AboutFragment.kt
+++ app/src/main/java/com/glucode/about_you/about/AboutFragment.kt
@@ -6,10 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.glucode.about_you.about.views.QuestionCardView
+import com.glucode.about_you.about.views.SimpleCardView
 import com.glucode.about_you.databinding.FragmentAboutBinding
 import com.glucode.about_you.mockdata.MockData
 
-class AboutFragment: Fragment() {
+
+class AboutFragment : Fragment() {
     private lateinit var binding: FragmentAboutBinding
 
     override fun onCreateView(
@@ -24,14 +26,20 @@ class AboutFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setUpSimpleCard()
         setUpQuestions()
+    }
+
+    private fun setUpSimpleCard() {
+        val simpleCardView = SimpleCardView(requireContext())
+        binding.container.addView(simpleCardView, 0) // Add it at the top of the container
     }
 
     private fun setUpQuestions() {
         val engineerName = arguments?.getString("name")
-        val engineer = MockData.engineers.first { it.name == engineerName }
+        val engineer = MockData.engineers.firstOrNull { it.name == engineerName }
 
-        engineer.questions.forEach { question ->
+        engineer?.questions?.forEach { question ->
             val questionView = QuestionCardView(requireContext())
             questionView.title = question.questionText
             questionView.answers = question.answerOptions
